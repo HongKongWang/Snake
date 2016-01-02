88851e33fd180fe29f9c2a17f13565c44e65dca5
HongKongWang <2826509864@qq.com>
2016-01-02 14:47:45 +0800

    Update Snake.java
    The Snake is Runable now ! The only difference is everywhere is different !

--- Snake.java
+++ Snake.java
@@ -8,16 +8,16 @@ import java.util.TreeMap;
 import javax.swing.*;
 
 /*
-	No long ago, I saw one of my friend(MarcoZhang) play Python.
-	That's why I had an idea to create a language simple as Python.
-	These words were all typed by me in only one day. So they will be changed soon~
+	No long ago, I saw one of my friends (MarcoZhang) play Python.
+	That's why I had an idea to create a language as simple as Python.
+	These words were all typed by me in only two days. So they will be changed soon~
 */
 public class Snake {
 	static TreeMap<String,Object> obj;
 	static JTextArea jta1;
+	static JButton jb3;
 	static JScrollPane sp;
 	static boolean sleep;
-	static slp SLP;
 	public static void main(String[] args) {
 		obj=new TreeMap<String, Object>();
 		JFrame jf=new JFrame("Snake");
@@ -30,10 +30,7 @@ public class Snake {
 		jf.setResizable(false);
 		jf.setBounds(350,74,517,609);
 		jta1=new JTextArea("Snake waked up......\n");
-		final JTextArea jtf2=new JTextArea();
-		jf.add(jtf2);
-		jtf2.setBounds(34,437,442,107);
-		JButton jb3=new JButton("Bit");
+		jb3=new JButton("Bite");
 		jf.add(jb3);
 		jb3.setBounds(33,397,442,30);
 		sp=new JScrollPane(jta1);
@@ -43,74 +40,94 @@ public class Snake {
 		jp1.setLayout(gl);
 		jp1.add(sp);
 		jf.add(jp1);
+		final JTextArea jtf2=new JTextArea();
+		JPanel jp2=new JPanel();
+		jp2.setBounds(34,437,442,107);
+		jp2.setLayout(gl);
+		JScrollPane sp1=new JScrollPane(jtf2);
+		jp2.add(sp1);
+		jf.add(jp2);
 		jf.setVisible(true);
 		jta1.setEditable(false);
 		jb3.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				String str=jtf2.getText();
 				jtf2.setText(null);
-				BufferedReader br=new BufferedReader(new StringReader(str));
-				String s = null;
 				jta1.append("I:");
-				if(str.isEmpty()){
+				str=str.replaceAll("\n","");
+				String[] s=str.split(";");
+				if(s[0].equals("")){
 					jta1.append("nothing\n");
+				}else{
+					if(str.endsWith(";")){
+						StringBuffer sb=new StringBuffer(str);
+						sb.setLength(sb.length()-1);
+						str=sb.toString();
+					}
+					jta1.append(str.replaceAll(";","\n  ")+"\nSnake:");
+					jtaWriter jw=new jtaWriter(jta1);
+					jw.read(s);
+					jw=null;
 					JScrollBar sb=sp.getVerticalScrollBar();
 					sb.setValue(sb.getMaximum());
-					say="";
-				}else{
-					while(true){
-						try {
-							s =br.readLine();
-						} catch (Exception e1) {}
-						if(s!=null){
-							jta1.append(s+"\n");
-							read(s);
-						}else{
-							break;
-						}
-					}
-					say();
 				}
 			}
 		});
 	}
-	static void say(String str){
-		say=say+"      "+str+"\n";
+}
+static class jtaWriter extends Thread{
+	JTextArea jta;
+	boolean head;
+	String[] str;
+	jtaWriter(JTextArea jta){
+		this.jta=jta;
+		this.head=true;
 	}
-	static void say(){
-		jta1.append("Snake:"+say.replaceFirst("      ",""));
-		JScrollBar sb=sp.getVerticalScrollBar();
-		sb.setValue(sb.getMaximum());
-		say="";
+	void say(String str){
+		if(head){
+			jta.append(str+"\n");
+			head=false;
+		}else{
+			jta.append("      "+str+"\n");
+		}
 	}
-	static void read(String str){
-		if(!sleep){
-			String[] fir=str.split("<<");
+	public void run(){
+		Snake.jb3.setEnabled(false);
+		for(int I=0;I<str.length;I++){
+			String[] fir=str[I].split("<<");
 			if(fir.length==2){
 				if(fir[0].equals("out")){
 					if(obj.containsKey(fir[1])){
-						say((String)obj.get(fir[1]));
+						say(obj.get(fir[1]).toString());
 					}else if(fir[1].startsWith("~")){
 						say(fir[1].replaceFirst("~",""));
 					}else{
-						say("Object No Found!");
+						try {
+							say(new Calculator().calculate(fir[1])+"");
+						} catch (Exception e) {
+							say("Calculate failed!");
+						}
 					}
 				}else if(fir[0].equals("sleep")){
 					Integer i=0;
-					try{
-						if(fir[1].startsWith("`")){
-							i=new Integer(fir[1].replaceFirst("`",""));
-							SLP=new slp(i);
-							SLP.start();
-						}else if(obj.get(fir[1])!=null){
-							i=new Integer((String)obj.get(fir[1]));
-							SLP=new slp(i);
-							SLP.start();
+						if(obj.get(fir[1])!=null){
+							try{
+								i=new Integer((String)obj.get(fir[1]));
+								sleep(i*1000);
+							} catch (InterruptedException e) {}
+							catch(NumberFormatException e1){
+								say("Calculate failed");
+							}
 						}else{
-							say("Time No Found!");
+							try {
+								i=new Integer((int) new Calculator().calculate(fir[1])+"");
+								sleep(i*1000);
+						}catch (InterruptedException e) {
+							e.printStackTrace();
 						}
-					}catch(Exception e){
-						say("Time No Found!");
+						catch (NumberFormatException e) {
+							say("Calculate failed!");
+						}
 					}
 				}else if(fir[0].equals("ascii")){
 					if(fir[1].startsWith("~")){
@@ -121,50 +138,56 @@ public class Snake {
 						}
 					}else if(obj.get(fir[1])!=null){
 						char[] c=((String) obj.get(fir[1])).toCharArray();
-						for(int i=0;i<fir[1].length();i++){
+						for(int i=0;i<((String) obj.get(fir[1])).length();i++){
 							Integer in=(int) c[i];
 							say(in.toString());
 						}
 					}else{
 						say("Object No Found!");
 					}
-				}else if(!fir[0].equals(null)){
+				}else if(noo(fir[0])){
 					if(fir[1].startsWith("~")){
 						obj.put(fir[0],fir[1].replaceFirst("~",""));
 						say(fir[1].replaceFirst("~",""));
 					}else if(obj.get(fir[1])!=null){
 						obj.put(fir[0],obj.get(fir[1]));
-						say((String) obj.get(fir[1]));
+						say(""+obj.get(fir[1]));
 					}else{
-						say("Object No Found!");
+						try {
+							say(new Calculator().calculate(fir[1])+"");
+							obj.put(fir[0],new Calculator().calculate(fir[1]));
+						} catch (Exception e) {
+							say("Calculate failed!");
+						}
 					}
 				}else{
-					say("No Object Name!");
+					say("Bad Object Name!");
 				}
 			}else{
 				say("Error!");
 			}
-		}else if(str.equals("wake up")){
-			SLP.interrupt();
 		}
+		head=true;
+		Snake.jb3.setEnabled(true);
 	}
-}
-class slp extends Thread{
-	Integer i;
-	slp(Integer i){
-		this.i=i;
-	};
-	public void run() {
-		try {
-			Snake.sleep=true;
-			sleep(i*1000);
-		} catch (InterruptedException e) {
-			
-		}
-		finally{
-			Snake.sleep=false;
-			Snake.jta1.append("Snake waked up......\n");
-
+	void read(String[] str){
+		this.str=str;
+		this.start();
+	}
+	boolean noo(String name){
+		char[] c=name.toCharArray();
+		if(!((c.length)==0)){
+				for(int i=0;i<c.length;i++){
+					if(!(((int)c[0]>=65&&(int)c[0]<=90)
+							|((int)c[0]>=97&&(int)c[0]<=122)
+							|((int)c[0]>=48&&(int)c[0]<=57)
+							|(int)c[0]==95)){
+						return false;
+					}
+				}
+				return true;
+		}else{
+			return false;
 		}
 	}
 }
