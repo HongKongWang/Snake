f376ef35b0ceb4bf495d8c750b75d69820e9e441
HongKongWang <2826509864@qq.com>
2016-01-09 13:56:35 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,8 @@ import java.util.Collections;
 import java.util.Stack;
 
         /*This is a file which I copied from the network, 
-        this class help Snake to calculate.*/
+        but I have remade it so that it can use values from Snake as well as calculate pow
+        this class helps Snake to calculate.*/
 public class Calculator {
     private Stack<String> postfixStack  = new Stack<String>();//后缀式栈
     private Stack<Character> opStack  = new Stack<Character>();//运算符栈
@@ -69,37 +70,16 @@ public class Calculator {
             postfixStack.push(String.valueOf( opStack.pop()));//将操作符栈中的剩余的元素添加到后缀式栈中
         }
     }
-    
-    /**
-     * 判断是否为算术符号
-     * @param c
-     * @return
-     */
     private boolean isOperator(char c) {
-        return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' ||c == ')';
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' ||c == ')'|| c=='^';
     }
-    
-    /**
-     * 利用ASCII码-40做下标去算术符号优先级
-     * @param cur
-     * @param peek
-     * @return
-     */
-    public  boolean compare(char cur,char peek) {// 如果是peek优先级高于cur，返回true，默认都是peek优先级要低
+    public  boolean compare(char cur,char peek) {//Compare the priority
         boolean result  = false;
         if(operatPriority[(peek)-40] >= operatPriority[(cur) - 40]) {
            result = true;
         }
         return result;
     }
-    
-    /**
-     * 按照给定的算术运算符做计算
-     * @param firstValue
-     * @param secondValue
-     * @param currentOp
-     * @return
-     */
     private String calculate(String firstValue,String secondValue,char currentOp) {
         String result  = "";
         switch(currentOp) {
@@ -115,142 +95,98 @@ public class Calculator {
             case '/':
                 result = String.valueOf(ArithHelper.div(firstValue, secondValue));
                 break;
+            case '^':
+            	result = String.valueOf(ArithHelper.ChengFang(firstValue, secondValue));
         }
         return result;
     }
 }
 class ArithHelper {
-
-    // 默认除法运算精度
-    private static final int DEF_DIV_SCALE = 16;
-
-    // 这个类不能实例化
     private ArithHelper() {
     }
-
-    /**
-     * 提供精确的加法运算。
-     * 
-     * @param v1 被加数
-     * @param v2 加数
-     * @return 两个参数的和
-     */
-
-    public static double add(double v1, double v2) {
-        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
-        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
-        return b1.add(b2).doubleValue();
-    }
-    
     public static double add(String v1, String v2) {
-        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
-        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
-        return b1.add(b2).doubleValue();
+    	double re=0;
+    	double d1=0;
+    	double d2=0;
+    	try{
+    		d1=new Double(v1);
+    	}catch(NumberFormatException e){
+    		d1=new Double(Snake.obj.get(v1));
+    	}
+    	try{
+    		d2=new Double(v2);
+    	}catch(NumberFormatException e){
+    		d2=new Double(Snake.obj.get(v2));
+    	}
+    	re=d1+d2;
+		return re;
     }
-
-    /**
-     * 提供精确的减法运算。
-     * 
-     * @param v1 被减数
-     * @param v2 减数
-     * @return 两个参数的差
-     */
-
-    public static double sub(double v1, double v2) {
-        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
-        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
-        return b1.subtract(b2).doubleValue();
-    }
-    
     public static double sub(String v1, String v2) {
-        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
-        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
-        return b1.subtract(b2).doubleValue();
+    	double re=0;
+    	double d1=0;
+    	double d2=0;
+    	try{
+    		d1=new Double(v1);
+    	}catch(NumberFormatException e){
+    		d1=new Double(Snake.obj.get(v1));
+    	}
+    	try{
+    		d2=new Double(v2);
+    	}catch(NumberFormatException e){
+    		d2=new Double(Snake.obj.get(v2));
+    	}
+    	re=d1-d2;
+		return re;
     }
-
-    /**
-     * 提供精确的乘法运算。
-     * 
-     * @param v1
-     *            被乘数
-     * @param v2
-     *            乘数
-     * @return 两个参数的积
-     */
-
-    public static double mul(double v1, double v2) {
-        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
-        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
-        return b1.multiply(b2).doubleValue();
-    }
-    
     public static double mul(String v1, String v2) {
-        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
-        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
-        return b1.multiply(b2).doubleValue();
+    	double re=0;
+    	double d1=0;
+    	double d2=0;
+    	try{
+    		d1=new Double(v1);
+    	}catch(NumberFormatException e){
+    		d1=new Double(Snake.obj.get(v1));
+    	}
+    	try{
+    		d2=new Double(v2);
+    	}catch(NumberFormatException e){
+    		d2=new Double(Snake.obj.get(v2));
+    	}
+    	re=d1*d2;
+		return re;
     }
-
-    /**
-     * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到 小数点以后10位，以后的数字四舍五入。
-     * 
-     * @param v1
-     *            被除数
-     * @param v2
-     *            除数
-     * @return 两个参数的商
-     */
-
-    public static double div(double v1, double v2) {
-        return div(v1, v2, DEF_DIV_SCALE);
-    }
-    
     public static double div(String v1, String v2) {
-        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
-        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
-        return b1.divide(b2, DEF_DIV_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
+    	double re=0;
+    	double d1=0;
+    	double d2=0;
+    	try{
+    		d1=new Double(v1);
+    	}catch(NumberFormatException e){
+    		d1=new Double(Snake.obj.get(v1));
+    	}
+    	try{
+    		d2=new Double(v2);
+    	}catch(NumberFormatException e){
+    		d2=new Double(Snake.obj.get(v2));
+    	}
+    	re=d1/d2;
+		return re;
     }
-
-    /**
-     * 提供（相对）精确的除法运算。当发生除不尽的情况时，由scale参数指 定精度，以后的数字四舍五入。
-     * 
-     * @param v1 被除数
-     * @param v2 除数
-     * @param scale 表示表示需要精确到小数点以后几位。
-     * @return 两个参数的商
-     */
-
-    public static double div(double v1, double v2, int scale) {
-        if (scale < 0) {
-            throw new IllegalArgumentException("The   scale   must   be   a   positive   integer   or   zero");
-        }
-        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
-        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
-        return b1.divide(b2, scale, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
-    }
-
-    /**
-     * 提供精确的小数位四舍五入处理。
-     * 
-     * @param v 需要四舍五入的数字
-     * @param scale 小数点后保留几位
-     * @return 四舍五入后的结果
-     */
-
-    public static double round(double v, int scale) {
-        if (scale < 0) {
-            throw new IllegalArgumentException("The   scale   must   be   a   positive   integer   or   zero");
-        }
-        java.math.BigDecimal b = new java.math.BigDecimal(Double.toString(v));
-        java.math.BigDecimal one = new java.math.BigDecimal("1");
-        return b.divide(one, scale, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
-    }
-    
-    public static double round(String v, int scale) {
-        if (scale < 0) {
-            throw new IllegalArgumentException("The   scale   must   be   a   positive   integer   or   zero");
-        }
-        java.math.BigDecimal b = new java.math.BigDecimal(v);
-        java.math.BigDecimal one = new java.math.BigDecimal("1");
-        return b.divide(one, scale, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
+    public static double ChengFang(String v1, String v2) {
+    	double re=0;
+    	double d1=0;
+    	double d2=0;
+    	try{
+    		d1=new Double(v1);
+    	}catch(NumberFormatException e){
+    		d1=new Double(Snake.obj.get(v1));
+    	}
+    	try{
+    		d2=new Double(v2);
+    	}catch(NumberFormatException e){
+    		d2=new Double(Snake.obj.get(v2));
+    	}
+    	re=Math.pow(d1,d2);
+    	return re;
     }
 }
